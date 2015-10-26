ca8097034a00586d2d60d3904b554213557c237f
juanc975 <juanc975@hotmail.com>
2015-10-26 12:49:03 -0400

    actualizacion de estilos para responsivo

--- themes/telartes/js/mrc.js
+++ themes/telartes/js/mrc.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function(){
+	//alert("INGRESO AL JS");
     var onlyChild = jQuery(".node .field-collection-container");
     if (onlyChild.length == 1) {
         jQuery(onlyChild).addClass("onlychild");
@@ -12,4 +13,32 @@ jQuery(document).ready(function(){
         }
 
     }
+});
+jQuery(document).ready(function()
+{
+	jQuery('.l-header-region .responsive-menus').click(function(){ 
+	    //alert('Evento click sobre menu right');
+		jQuery(".top-bar").removeClass("expanded");
+		jQuery(".sidebar-first .responsive-menus").removeClass("responsive-toggled");
+	});
+	
+	jQuery('.sidebar-first .responsive-menus').click(function(){ 
+	    //alert('Evento click sobre menu lef');
+		jQuery(".top-bar").removeClass("expanded");
+		jQuery(".l-header-region .responsive-menus").removeClass("responsive-toggled");
+	});
+	jQuery('.top-bar').click(function(){ 
+	    //alert('Evento click sobre menu principal');
+		jQuery(".responsive-menus").removeClass("responsive-toggled");
+	});	
+	jQuery('.main').click(function(){ 
+	    //alert('Evento click sobre main');
+		jQuery(".responsive-menus").removeClass("responsive-toggled");
+		jQuery(".top-bar").removeClass("expanded");
+	});	
+	jQuery('.sidebar-second').click(function(){ 
+	    //alert('Evento click sobre main');
+		jQuery(".responsive-menus").removeClass("responsive-toggled");
+		jQuery(".top-bar").removeClass("expanded");
+	});
 });
